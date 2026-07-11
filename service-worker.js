5fb9b2dbd1e620fe79f36aa09c822ff3ddc98324
Yolkers <yolkers@example.com>
2026-07-11 09:41:01 -0400

    Initial Better Dad app

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE = 'better-dad-v1';
+const ASSETS = ['./index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
