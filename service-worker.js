815e00351f72fc21bdb0788278ae639566bdea15
lukebot <lukebot@admins-Mac-mini.local>
2026-07-11 12:00:12 -0400

    Add animated knight levels

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,19 @@
-const CACHE = 'better-dad-v3';
+const CACHE = 'better-dad-v4';
 const ASSETS = [
   './index.html',
   './manifest.webmanifest',
   './icon.svg',
-  './assets/warrior-base.svg',
-  './assets/warrior-leather.svg',
-  './assets/warrior-chain.svg',
-  './assets/warrior-shield.svg',
-  './assets/warrior-plate.svg',
-  './assets/warrior-legend.svg'
+  './assets/level-01.gif',
+  './assets/level-02.gif',
+  './assets/level-03.gif',
+  './assets/level-04.gif',
+  './assets/level-05.gif'
 ];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
 });
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
